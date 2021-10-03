026ef2c53d3db8b22775c6445e337227a79d0e0e
maximaCoder <62437851+maximaCoder@users.noreply.github.com>
2021-10-03 11:56:30 +0100

    fixed some validations errors for the add_friend mutation

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -183,14 +183,43 @@ const Mutation = mutationType({
         args: {
           friendId: nonNull(stringArg()),
         },
-        resolve: async (_parent, args, { prisma, userId }) => {
+        resolve: async (_parent, { friendId }, { prisma, userId }) => {
+          const isFriendExist = await prisma.user.findUnique({
+            where: { id: friendId },
+          })
+          if (!isFriendExist) {
+            throw new ValidationError('friend does not have an account')
+          }
+          const isFriendAlreadyExistInMyList = await prisma.user.findFirst({
+            where: {
+              AND: [
+                {
+                  id: userId,
+                },
+                {
+                  friends: {
+                    some: {
+                      id: {
+                        equals: friendId,
+                      },
+                    },
+                  },
+                },
+              ],
+            },
+          })
+          if (isFriendAlreadyExistInMyList) {
+            throw new ValidationError(
+              `${isFriendAlreadyExistInMyList.username} is already exist in your friendlist`,
+            )
+          }
           const updatedUser = await prisma.user.update({
             where: {
               id: userId,
             },
             data: {
               friends: {
-                connect: { id: args.friendId },
+                connect: { id: friendId },
               },
             },
           })
